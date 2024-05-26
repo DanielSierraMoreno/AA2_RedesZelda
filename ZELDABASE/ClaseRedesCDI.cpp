b99311491b1de1aebea48eedcea74988f5bfef47
Daniel Sierra Moreno <daniel.sierra@enti.cat>
2024-05-26 11:47:04 +0200

    a

--- ZELDABASE/ClaseRedesCDI.cpp
+++ ZELDABASE/ClaseRedesCDI.cpp
@@ -3,10 +3,33 @@
 #include <thread>
 #include "ResourcesClass/Player.h"
 #include "ResourcesClass/Window.h"
-#include "Game.h"
 
 
+class Game
+{
+public:
+    Game(Window* window)
+    {
+        _window = window;
+        _player = new Player(window);
+    }
+    ~Game()
+    {
+        delete _player;
+    }
 
+    void Run()
+    {
+        while (_window->IsOpen()) // Continue loop while window is open
+        {
+            _player->Update();
+        }
+    }
+
+private:
+    Window* _window;
+    Player* _player;
+};
 
 
 void RunWindows(Window* window)
